70e548908bd4b22c4b9b4390a9075281d21eace8
fabanc <fabien.ancelin@gmail.com>
2020-10-10 15:35:42 -0400

    Ability to add / remove elements to list

--- src/common/groceryApp.jsx
+++ src/common/groceryApp.jsx
@@ -8,12 +8,31 @@ import ListGroup from 'react-bootstrap/ListGroup'
 export default class App extends React.Component {
 
   state = {
-      data: []
+      data: [],
   }
+
+  // constructor(props){
+  //   this.selectionHandler = this.setSelected.bind(this);
+  // }
 
   addItem(item){
     console.log(item)
   }
+
+  setSelected = (index) => {
+      console.log('Selected Index: ', index)
+      let localData = this.state.data;
+      let selectionValue = this.state.data[index].selected ? false : true
+      console.log('setting selected to ', selectionValue, " for ", localData[index].name)
+      localData[index].selected = selectionValue;
+      this.setState({
+        data: localData
+      }, 
+        ()=> console.log(this.props.selected)
+      );
+      
+  }
+
 
   render(){
     let elements = this.state.data;
@@ -21,34 +40,53 @@ export default class App extends React.Component {
         console.log("Print element");
         return (<GroceryItem name={element}/>)
     })
+
+    let selectionHandler = this.setSelected.bind(this)
+
+
     return (
       <div className="App">
-        {/* <header className="App-header">    
-        </header> */}
+        {
+          <header className="App-header">Welcome to your Westboro Store grocery sorted </header>}
         <div class="half-width">
+          <h3>Choose your products</h3>
           <ListGroup>
-          {this.state.data.map((element)=>{
-            return (<GroceryItem key={element} name={element} addClick={() => this.addItem(element)}> Something </GroceryItem>)
+          {this.state.data.map((element, index)=>{
+            return (<GroceryItem key={element.name} name={element.name} selected={element.selected} index={index} setSelected={selectionHandler} > Something </GroceryItem>)
           })}
           </ListGroup>
         </div>
       </div>
-
-
     );
   }
 
-
   setStoreData(data){
+    console.log("Start: ", data.data.starts[0])
+    console.log("End: ", data.data.ends[0])
     let points = data.data.points.filter((currentValue, index)=> {
-        return index !== data.data.starts[0] && data.data.ends[0] 
+        return index !== data.data.starts[0] && index !== data.data.ends[0] 
     })
-    this.setState({ data: points });
+
+    let point_data = points.map((element, index) => {
+        {
+          return {name: element, selected: false, odindex: index}
+        }
+    });
+
+    point_data.sort(function(a,b){
+      if(a.name > b.name) return 1;
+      if(a.name < b.name) return -1;
+      return 0
+    })
+
+    console.log(points)
+
+    this.setState({ data: point_data });
   }
 
   componentDidMount() {
     console.log("Component did mount");
-    fetch('https://grocerysorterapp.azurewebsites.net/get_store_info')
+    fetch('https://grocerysorterwebapp.azurewebsites.net/get_store_info')
     .then(response => response.json())
     .then(data => this.setStoreData({ data }));
   }
--- src/groceryList/groceryItem.jsx
+++ src/groceryList/groceryItem.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from 'react-bootstrap/Button'
 import ListGroup from 'react-bootstrap/ListGroup'
 
   export default class GroceryItem extends React.Component{
-    render(){
 
+    // constructor(props){
+    //   super(props);
+    // }
+    
+    // setSelected = () => {
+    //   let selectionValue = this.props.selected ? false : true
+    //   console.log('setting selected to ', selectionValue, " to ", this.props.name)
+    //   this.setState({
+    //     selected: selectionValue
+    //   }, 
+    //     ()=> console.log(this.props.selected)
+    //   );
+      
+    // }
+
+    getButtonText(){
+      let text= this.props.selected ? "Remove" : "Add";
+      return (<Button variant="primary" onClick={ () => this.props.setSelected(this.props.index)}>{text}</Button>)
+    }
+
+    render(){
+      
+      let mybutton = this.getButtonText();
       return(
 
             <ListGroup.Item>{this.props.name}
-              <Button variant="primary" onClick={(e)=> console.log(this.props.name)}>Add</Button>
+              {mybutton}
             </ListGroup.Item>
       )
     }
