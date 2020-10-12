fc82c5f713a9f8c2a0687519318dd3d5e0cf5cf1
fabanc <fabien.ancelin@gmail.com>
2020-10-12 12:25:47 -0400

    Updated app description

--- src/about/index.jsx
+++ src/about/index.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 
 export default function About() {
-  const authors = [
-    { id: 1, name: 'Fabien Ancelin' },
-  ];
+  
   return (
     <>
       <h1>About page</h1>
-      <p>This is the About page. And here are the authors:</p>
-      <ul>
-        {authors.map(author => (
-          <li key={author.id}>{author.name}</li>
-        ))}
-      </ul>
+      <p>This project has been developped by Fabien Ancelin. Follow my on <a href="https://github.com/fabanc">GitHub</a></p>
+
     </>
   );
 }
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -3,8 +3,19 @@ import React from 'react';
 export default function Home() {
   return (
     <>
-      <h1>Home page</h1>
-      <p>This is the home page!</p>
+      <div className="row"><h1>Home page</h1></div>
+      <div className="row">
+        <p >Welcome to the grocey sorter application. This is application is a pet project designed to sort my grocery during the pandemic. I shop at the Real Canadian SuperStore in Westboro, Ottawa: <a href="https://goo.gl/maps/41beByV9swQ6qx1G7">Click here see in Google Maps</a>  If you shop here, feel free to use that tool</p>
+
+        <p >The map of the store is done using Esri's product, called ArcGIS. The back end is all provided using micro-services hosted in Azure. My goal was to have a tool that allows me to:
+        </p>
+
+        <ul>
+            <li>Get my grocery done fast, without using Online system (leave availble for the vulnerable folks)</li>
+            <li>Experiment with Azure Services</li>
+            <li>Practive my skills with the React and Bootstrap APIs</li>
+          </ul>
+      </div>
     </>
   );
 }
